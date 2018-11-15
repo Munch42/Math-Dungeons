1d8151be7248be58ad29743977016bede0523d51
Munch42 <Munch4242@gmail.com>
2018-11-14 20:25:20 -0700

    Added Animations to Player and Monsters

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,14 +8,28 @@ public class PlayerController : MonoBehaviour {
 
     public Animation playerInjured;
     public Animation monsterInjured;
+    public Text monsterInjureText;
+    public Text playerInjureText;
 
-	// Use this for initialization
-	void Start () {
+    private int monsterDamageDealt;
+    private int playerDamageDealt;
 
+    // Use this for initialization
+    void Start () {
+        playerInjureText.text = "";
+        monsterInjureText.text = "";
     }
 	
 	// Update is called once per frame
 	void Update () {
-
+        /*
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            playerInjured.Play();
+            monsterInjured.Play();
+            playerInjureText.text = "155155";
+            monsterInjureText.text = "155555";
+        }
+        */
 	}
 }
