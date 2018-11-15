57ce0c43183326908529956963e7aca8e4159e84
Munch42 <Munch4242@gmail.com>
2018-11-14 20:16:47 -0700

    Added Text Animations and Monster Spawning
    Added text animations but they are not on anything yet.

---
+++ Assets/Scripts/MonsterSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour {
+
+    public GameObject ImpMonsterPrefab;
+    public GameObject TrollMonsterPrefab;
+    public GameObject DragonMonsterPrefab;
+
+    private Vector2 monsterSpawnPoint;
+
+	// Use this for initialization
+	void Start () {
+        monsterSpawnPoint = transform.position;
+
+        int monsterNum = Random.Range(1,4);
+
+        if (monsterNum == 1)
+        {
+            Instantiate(ImpMonsterPrefab, monsterSpawnPoint, Quaternion.identity);
+            //Debug.Log(monsterNum);
+        }
+
+        if (monsterNum == 2)
+        {
+            Instantiate(TrollMonsterPrefab, monsterSpawnPoint, Quaternion.identity);
+            //Debug.Log(monsterNum);
+        }
+
+        if (monsterNum == 3)
+        {
+            Instantiate(DragonMonsterPrefab, monsterSpawnPoint, Quaternion.identity);
+            //Debug.Log(monsterNum);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public Animation playerInjured;
+    public Animation monsterInjured;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
