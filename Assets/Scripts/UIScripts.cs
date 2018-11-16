6900327bb375809dbbe1259f0452ca57369e3ed0
Munch42 <Munch4242@gmail.com>
2018-11-15 18:07:14 -0700

    Added Button Navigation

---
+++ Assets/Scripts/UIScripts.cs
@@ -0,0 +1,83 @@
+﻿using  System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScripts : MonoBehaviour {
+
+    //Coming Soon Text only until feature is complete
+    public GameObject comingSoonGameobject;
+
+    //Back Button
+    public GameObject backButton;
+
+    //Main Screen Buttons
+    public GameObject attackButton;
+    public GameObject petButton;
+    public GameObject specialButton;
+    public GameObject retreatButton;
+
+    //Attack Buttons
+    public GameObject attackButtons;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public void OnAttackClick()
+    {
+        attackButton.SetActive(false);
+        petButton.SetActive(false);
+        specialButton.SetActive(false);
+        retreatButton.SetActive(false);
+
+        attackButtons.SetActive(true);
+        backButton.SetActive(true);
+    }
+
+    public void OnSpecialClick()
+    {
+        attackButton.SetActive(false);
+        petButton.SetActive(false);
+        specialButton.SetActive(false);
+        retreatButton.SetActive(false);
+
+        comingSoonGameobject.SetActive(true);
+        backButton.SetActive(true);
+    }
+
+    public void OnPetClick()
+    {
+        attackButton.SetActive(false);
+        petButton.SetActive(false);
+        specialButton.SetActive(false);
+        retreatButton.SetActive(false);
+
+        comingSoonGameobject.SetActive(true);
+        backButton.SetActive(true);
+    }
+
+    public void OnRetreatClick()
+    {
+        attackButton.SetActive(false);
+        petButton.SetActive(false);
+        specialButton.SetActive(false);
+        retreatButton.SetActive(false);
+
+        comingSoonGameobject.SetActive(true);
+        backButton.SetActive(true);
+    }
+
+    public void OnBackClick()
+    {
+        attackButton.SetActive(true);
+        petButton.SetActive(true);
+        specialButton.SetActive(true);
+        retreatButton.SetActive(true);
+
+        attackButtons.SetActive(false);
+        comingSoonGameobject.SetActive(false);
+        backButton.SetActive(false);
+    }
+}
