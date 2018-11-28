4c1e69403f2994747ea6a94f96cc9253b718a1c7
Munch42 <Munch4242@gmail.com>
2018-11-27 20:34:17 -0700

    Added NAC Logo Splash Screen

---
+++ Assets/Scripts/IntroTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroTimer : MonoBehaviour {
+
+    public GameObject nacLogo;
+
+    private int timer = 240;
+
+	// Use this for initialization
+	void Start () {
+        nacLogo.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timer > 0)
+        {
+            timer--;
+        }
+        else if (timer <= 0)
+        {
+            nacLogo.SetActive(false);
+            SceneManager.LoadScene("TitleScreen", LoadSceneMode.Single);
+        }
+	}
+}
